edc16970d1b06dc2b4f514d143ffaf8260e1caa5
Konstantin Savchenko <konstantine.sa@gmail.com>
2023-03-29 20:43:49 +0200

    Added egg type selection functionality in JS

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
-const egg_small = document.querySelector(".egg--size-s");
+//global variables
+const weightRatio = calculateWeightRatio();
+var yolkTypeRatio = 0;
+var cookingTime = weightRatio + yolkTypeRatio;
 
-egg_small.onclick = function () {
-  egg_small.classList.add = "egg--selected";
-};
+const eggSize = document.querySelectorAll(".egg");
+const yolkType = document.querySelectorAll(".yolk-type__item ");
+
+function selectEggSize() {
+  // looping through each egg
+  eggSize.forEach((egg) => {
+    // Adding click event listener to each egg
+    egg.addEventListener("click", () => {
+      // Removing the "selected" class from all eggs
+      eggSize.forEach((egg) => {
+        egg.classList.remove("egg--selected");
+      });
+
+      // Adding the "selected" class to the clicked egg
+      egg.classList.add("egg--selected");
+
+      //getting weight of the selected egg
+      const weightRatio = calculateWeightRatio();
+      console.log("Weight Ratio: " + weightRatio); //*******************************TO REMOVE AFTER DEBUGGING!*****************************
+    });
+  });
+}
+
+function selectYolkType() {
+  yolkType.forEach((egg) => {
+    egg.addEventListener("click", () => {
+      yolkType.forEach((egg) => {
+        egg.classList.remove("yolk-type__item--selected");
+      });
+
+      egg.classList.add("yolk-type__item--selected");
+
+      yolkTypeRatio = parseFloat(egg.dataset.value);
+      console.log("YolkType Ratio: " + yolkTypeRatio); //*******************************TO REMOVE AFTER DEBUGGING!*****************************
+    });
+  });
+}
+
+//getting weight of the selected egg
+function calculateWeightRatio() {
+  let weightRatio = 1.16667;
+  const selectedEgg = document.querySelector(".egg--selected");
+
+  if (selectedEgg.classList.contains("egg--size-s")) {
+    weightRatio = 1;
+  } else if (selectedEgg.classList.contains("egg--size-m")) {
+    weightRatio = 1.16667;
+  } else if (selectedEgg.classList.contains("egg--size-l")) {
+    weightRatio = 1.33333;
+  } else if (selectedEgg.classList.contains("egg--size-xl")) {
+    weightRatio = 1.5;
+  }
+
+  return weightRatio;
+}
+
+selectEggSize();
+selectYolkType();
 
 //Timer module
 const semicircles = document.querySelectorAll(".semicircle");
@@ -10,7 +68,7 @@ const timer = document.querySelector(".timer");
 
 //input
 const min = 0;
-const sec = 15;
+const sec = cookingTime;
 
 const minutes = min * 60000;
 const seconds = sec * 1000;
